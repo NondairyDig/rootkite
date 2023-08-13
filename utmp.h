870595338d30e29b3198a4930738f74792169562
NondairyDig <t.shlomi@outlook.com>
2023-08-13 06:45:54 +0000

    __WORD_COMPAT32

--- utmp.h
+++ utmp.h
@@ -50,7 +50,7 @@ struct utmp
 /* The ut_session and ut_tv fields must be the same size when compiled
    32- and 64-bit.  This allows data files and shared memory to be
    shared between 32- and 64-bit applications.  */
-#if __WORDSIZE_TIME64_COMPAT32
+#if __WORDSIZE_TIME64_COMPAT32 || defined __WORDSIZE_COMPAT32
   int32_t ut_session;           /* Session ID, used for windowing.  */
   struct
   {
